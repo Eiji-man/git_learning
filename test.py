8712d7648d9b983b6f08a578f5a44e0b9ca12e1d
EijiYanai <2117368@mchcgr.com>
2024-09-17 17:07:08 +0900

    fix ruffルールに準拠

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import os
 import sys
-import io  # 問題点1:1行に複数のインポート 問題点2:ioをインポートしているが未使用
+# import io  # 問題点1:1行に複数のインポート 問題点2:ioをインポートしているが未使用
 
 
 def greetingPath(msg, name):  # 命名規則にエラー。後述のオプションで説明
     print(sys.argv)
     path = os.get_exec_path()  # 問題点3:path変数が未使用
-    print(f"{msg}, {name}")
+    print(f"{msg}, {name}", {path})
